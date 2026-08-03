e7ca3726fa5a363143d7cd7b29766561e0b62511
wmranjit11-commits <wmranjit11@gmail.com>
2026-08-03 14:45:34 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Oswald, Roboto_Condensed } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
+import { ContactModal } from "@/components/common/ContactModal";
 
 const headingFont = Oswald({
   subsets: ["latin"],
@@ -19,17 +21,39 @@ export const metadata: Metadata = {
     "Pithal Machine Pvt. delivers high-performance crushing, screening, mining, and industrial machinery solutions.",
 };
 
-import { ContactModal } from "@/components/common/ContactModal";
-
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${headingFont.variable} ${bodyFont.variable}`}>
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NQNJCS84');`,
+          }}
+        />
+      </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NQNJCS84"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <ContactModal />
       </body>
     </html>
   );
 }
+
